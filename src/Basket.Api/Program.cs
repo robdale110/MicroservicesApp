dd9edec0d7a0591def0d92d59df2e1d7d7424a28
Rob Dale <rob@robdale.me.uk>
2021-04-18 17:30:22 +0100

    Added to docker

--- src/Basket.Api/Program.cs
+++ src/Basket.Api/Program.cs
@@ -1,11 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Basket.Api
 {
@@ -20,7 +14,7 @@ namespace Basket.Api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>(); 
                 });
     }
 }
