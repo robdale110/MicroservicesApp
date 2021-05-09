b91b0383f8b2d4a038992b7d3783b91d521989ec
Rob Dale <rob@robdale.me.uk>
2021-05-09 15:45:36 +0100

    Finished shopping aggregator

---
+++ src/Shopping.Aggregator/Services/BasketService.cs
@@ -0,0 +1,23 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _client;
+
+        public BasketService(HttpClient client) =>
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+
+        public async Task<BasketModel> GetBasket(string userName)
+        {
+            var result = await _client.GetAsync($"/api/v1/basket/{userName}");
+
+            return await result.ReadContentAs<BasketModel>();
+        }
+    }
+}
---
+++ src/Shopping.Aggregator/Services/CatalogService.cs
@@ -0,0 +1,38 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        private readonly HttpClient _client;
+
+        public CatalogService(HttpClient client) =>
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+
+        public async Task<IEnumerable<CatalogModel>> GetCatalog()
+        {
+            var result = await _client.GetAsync("/api/v1/catalog");
+
+            return await result.ReadContentAs<List<CatalogModel>>();
+        }
+
+        public async Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
+        {
+            var result = await _client.GetAsync($"/api/v1/catalog/GetProductByCategory/{category}");
+
+            return await result.ReadContentAs<List<CatalogModel>>();
+        }
+
+        public async Task<CatalogModel> GetCatalog(string id)
+        {
+            var result = await _client.GetAsync($"/api/v1/catalog/{id}");
+
+            return await result.ReadContentAs<CatalogModel>();
+        }
+    }
+}
